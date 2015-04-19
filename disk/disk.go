eb1b69ea9517d91d977d6268439c1c0433fc8ea2
Yutong Pei <yutong@upthere.com>
2015-04-19 13:43:51 -0700

    nil return

--- disk/disk.go
+++ disk/disk.go
@@ -3,9 +3,9 @@ package disk
 import "io"
 
 func ReadBlock(file io.ReadSeeker, index, bs int64) ([]byte, error) {
-
+	return nil, nil
 }
 
 func WriteBlock(file io.WriteSeeker, index, bs int64, data []byte) error {
-
+	return nil
 }
