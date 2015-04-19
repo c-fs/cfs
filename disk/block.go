d84ecdc36b1fd9e3dddc19ecaa9c60c2bbc129bf
Xiang Li <xiangli.cs@gmail.com>
2015-04-19 14:58:58 -0700

    disk: loosen the checking in writeBlock

--- disk/block.go
+++ disk/block.go
@@ -9,17 +9,19 @@ import (
 )
 
 var (
-	crc32cTable       = crc32.MakeTable(crc32.Castagnoli)
-	crc32Len          = int64(4)
-	ErrBadPayloadSize = errors.New("disk: bad payload size")
+	crc32cTable            = crc32.MakeTable(crc32.Castagnoli)
+	crc32Len               = int64(4)
+	ErrPayloadSizeTooLarge = errors.New("disk: payload size too large")
 )
 
-// writeBlock writes a full block into ws. len(p) must be (bs - crc32Len).
+// writeBlock writes a full block into ws. len(p) must be smaller than (bs - crc32Len).
 // Each block contains a crc32Len bytes crc32c of the payload and a (bs-crc32Len)
 // bytes payload. Any error encountered is returned.
+// It is caller's responsibility to ensure that only the last block in the file has
+// len(p) < bs - crc32Len
 func writeBlock(ws io.WriteSeeker, index, bs int64, p []byte) error {
-	if int64(len(p)) != bs-crc32Len {
-		return ErrBadPayloadSize
+	if int64(len(p)) > bs-crc32Len {
+		return ErrPayloadSizeTooLarge
 	}
 
 	// seek to the beginning of the block
