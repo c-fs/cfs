8351d0c1d30571bbfa466aab8b6dc07d0ee92ca7
Yutong Pei <yutong@upthere.com>
2015-06-01 09:21:56 -0700

    changes based on Xiang's comments

--- client/client.go
+++ client/client.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"errors"
-	"log"
 
 	pb "github.com/c-fs/cfs/proto"
 	"golang.org/x/net/context"
@@ -10,22 +9,21 @@ import (
 )
 
 type Client struct {
-	conn *grpc.ClientConn
+	grpcConn   *grpc.ClientConn
+	grpcClient pb.CfsClient
 }
 
-func New(address string) *Client {
+func New(address string) (*Client, error) {
 	conn, err := grpc.Dial(address)
 	if err != nil {
-		log.Fatalf("Cannot connect: %v", err)
+		return nil, err
 	}
-	return &Client{conn: conn}
+	pbClient := pb.NewCfsClient(conn)
+	return &Client{grpcConn: conn, grpcClient: pbClient}, nil
 }
 
-func (c *Client) Write(
-	ctx context.Context, name string, offset int64, data []byte, isAppend bool,
-) (int64, error) {
-	pbClient := pb.NewCfsClient(c.conn)
-	reply, err := pbClient.Write(
+func (c *Client) Write(ctx context.Context, name string, offset int64, data []byte, isAppend bool) (int64, error) {
+	reply, err := c.grpcClient.Write(
 		ctx,
 		&pb.WriteRequest{Name: name, Offset: offset, Data: data, Append: isAppend},
 	)
@@ -33,14 +31,12 @@ func (c *Client) Write(
 	if err != nil {
 		return 0, err
 	}
-	return reply.BytesWritten, pbErr2GoErr(reply.Error)
+	return reply.BytesWritten, parseErr(reply.Error)
 }
 
-func (c *Client) Read(
-	ctx context.Context, name string, offset, length int64, checkSum uint32,
+func (c *Client) Read(ctx context.Context, name string, offset, length int64, checkSum uint32,
 ) (int64, []byte, uint32, error) {
-	pbClient := pb.NewCfsClient(c.conn)
-	reply, err := pbClient.Read(
+	reply, err := c.grpcClient.Read(
 		ctx,
 		&pb.ReadRequest{
 			Name: name, Offset: offset, Length: length, ExpChecksum: checkSum,
@@ -50,12 +46,11 @@ func (c *Client) Read(
 	if err != nil {
 		return 0, nil, 0, err
 	}
-	return reply.BytesRead, reply.Data, reply.Checksum, pbErr2GoErr(reply.Error)
+	return reply.BytesRead, reply.Data, reply.Checksum, parseErr(reply.Error)
 }
 
 func (c *Client) Rename(ctx context.Context, oldName, newName string) error {
-	pbClient := pb.NewCfsClient(c.conn)
-	reply, err := pbClient.Rename(
+	reply, err := c.grpcClient.Rename(
 		ctx,
 		&pb.RenameRequest{Oldname: oldName, Newname: newName},
 	)
@@ -63,43 +58,40 @@ func (c *Client) Rename(ctx context.Context, oldName, newName string) error {
 	if err != nil {
 		return err
 	}
-	return pbErr2GoErr(reply.Error)
+	return parseErr(reply.Error)
 }
 
 func (c *Client) Remove(ctx context.Context, name string, all bool) error {
-	pbClient := pb.NewCfsClient(c.conn)
-	reply, err := pbClient.Remove(ctx, &pb.RemoveRequest{Name: name, All: all})
+	reply, err := c.grpcClient.Remove(ctx, &pb.RemoveRequest{Name: name, All: all})
 
 	if err != nil {
 		return err
 	}
-	return pbErr2GoErr(reply.Error)
+	return parseErr(reply.Error)
 }
 
 func (c *Client) ReadDir(ctx context.Context, name string) ([]*pb.FileInfo, error) {
-	pbClient := pb.NewCfsClient(c.conn)
-	reply, err := pbClient.ReadDir(ctx, &pb.ReadDirRequest{Name: name})
+	reply, err := c.grpcClient.ReadDir(ctx, &pb.ReadDirRequest{Name: name})
 
 	if err != nil {
 		return nil, err
 	}
-	return reply.FileInfos, pbErr2GoErr(reply.Error)
+	return reply.FileInfos, parseErr(reply.Error)
 }
 
 func (c *Client) Mkdir(ctx context.Context, name string, all bool) error {
-	pbClient := pb.NewCfsClient(c.conn)
-	reply, err := pbClient.Mkdir(ctx, &pb.MkdirRequest{Name: name, All: all})
+	reply, err := c.grpcClient.Mkdir(ctx, &pb.MkdirRequest{Name: name, All: all})
 
 	if err != nil {
 		return err
 	}
-	return pbErr2GoErr(reply.Error)
+	return parseErr(reply.Error)
 }
 
 func (c *Client) Close() {
-	c.conn.Close()
+	c.grpcConn.Close()
 }
 
-func pbErr2GoErr(pbErr *pb.Error) error {
+func parseErr(pbErr *pb.Error) error {
 	return errors.New(pbErr.String())
 }
