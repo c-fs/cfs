3980da8fe8ff9a53e4a23564c2d9ec37ba60fa4b
tuanjie <wangtuanjiezh@gmail.com>
2015-06-15 21:39:41 +0800

    cfs copy

--- server/util.go
+++ server/util.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"path"
 	"strings"
 )
@@ -23,4 +24,34 @@ func splitDiskAndFile(name string) (string, string, error) {
 		return "", "", fmt.Errorf("bad name: %s", name)
 	}
 	return names[0], names[1], nil
+}
+
+func NewOffReader(r func([]byte, int64) (int, error)) io.Reader {
+	return &offReader{r, 0}
+}
+
+func NewOffWriter(w func([]byte, int64) (int, error)) io.Writer {
+	return &offWriter{w, 0}
+}
+
+type offReader struct {
+	reader func([]byte, int64) (int, error)
+	off    int64
+}
+
+func (or *offReader) Read(b []byte) (int, error) {
+	n, err := or.reader(b, or.off)
+	or.off += int64(n)
+	return n, err
+}
+
+type offWriter struct {
+	writer func([]byte, int64) (int, error)
+	off    int64
+}
+
+func (ow *offWriter) Write(b []byte) (int, error) {
+	n, err := ow.writer(b, ow.off)
+	ow.off += int64(n)
+	return n, err
 }
